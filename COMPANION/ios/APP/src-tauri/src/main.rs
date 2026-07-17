72ef0c5efd7651020a189288bc9117e89559224b
Cello <cello@m6prod.com>
2026-07-17 20:22:14 -0300

    HARNESS-001 IOS

--- COMPANION/ios/APP/src-tauri/src/main.rs
+++ COMPANION/ios/APP/src-tauri/src/main.rs
@@ -1,6 +1,3 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
 fn main() {
-    kore_companion_ios_lib::run()
+    kore_companion_lib::run()
 }
